d50f847bcc9d40616031c6a0964eef195d0227bb
Mickaël Wolff <mickael@lupusmic.org>
2021-05-04 06:36:19 +0200

    Wordpress Object Cache interface

---
+++ src/WordPress/CacheObjectInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LupusMichaelis\WordPress;
+
+interface CacheObjectInterface
+{
+	const default_group_name = 'default';
+	const default_expires_in = 0;
+
+	function __construct();
+	function get($key, $group = self::default_group_name, $force = false, &$found = null);
+	function get_multi($groups);
+
+	function add($key, $data, $group = self::default_group_name, $expires = self::default_expires_in);
+	function add_global_groups($groups);
+	function add_non_persistent_groups($groups);
+
+	function replace($key, $data, $group = self::default_group_name, $expires = self::default_expires_in);
+
+	function incr($key, $bump = 1, $group = self::default_group_name);
+	function decr($key, $bump = 1, $group = self::default_group_name);
+
+	function delete($key, $group = self::default_group_name);
+	function flush();
+
+	function switch_to_blog($blog_id);
+	function close();
+}
