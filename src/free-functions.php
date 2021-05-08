b48191ab74cf68ecce4dfccc9e66c1c78acc4fc3
Mickaël Wolff <mickael@lupusmic.org>
2021-05-08 15:13:26 +0200

    Support optionnal parameters in method_parameters_as_map

--- src/free-functions.php
+++ src/free-functions.php
@@ -6,6 +6,7 @@ namespace LupusMichaelis\NestedCache;
  * @param $that object
  * @param $function_name string
  * @param $values &array
+ * @throws \ReflectionException	If array of values is too short and there is no default value
  * @return array[mixed]
  */
 function method_parameters_as_map(object $that, string $function_name, array & $values)
@@ -13,10 +14,15 @@ function method_parameters_as_map(object $that, string $function_name, array & $
 	$object_glimpse = new \ReflectionClass($that);
 	$function_glimpse = $object_glimpse->getMethod($function_name);
 	$parameter_list = $function_glimpse->getParameters();
-	$parameter_list = \array_map(function(\ReflectionParameter $parameter_glimpse)
-	{
-		return $parameter_glimpse->getName();
-	}, $parameter_list);
 
-	return \array_combine($parameter_list, $values);
+	$count_values = count($values);
+	$value_list = [];
+	foreach($parameter_list as $parameter_position => $parameter_glimpse)
+		$value_list[$parameter_glimpse->getName()] =
+			$parameter_position < $count_values
+				?  $values[$parameter_position]
+				: $parameter_glimpse->getDefaultValue()
+				;
+
+	return $value_list;
 }
--- tests/FreeFunctionsTest.php
+++ tests/FreeFunctionsTest.php
@@ -23,4 +23,36 @@ class FreeFunctionsTest
 
 		$this->assertEquals(['toto' => 1, 'tata' => 'plus', 'titi' => 'mieux'], $result);
 	}
+
+	public function testMethodParametersAsMapWithDefaultValues()
+	{
+		$c = new class
+			{
+				function something($toto, $tata = null, $titi = 'mieux')
+				{
+					$args = func_get_args();
+					return LMNC\method_parameters_as_map($this, __FUNCTION__, $args);
+				}
+			};
+
+		$result = $c->something(1, 'plus');
+
+		$this->assertEquals(['toto' => 1, 'tata' => 'plus', 'titi' => 'mieux'], $result);
+	}
+
+	public function testMethodParametersAsMapWithNoEnoughValues()
+	{
+		$c = new class
+			{
+				function something($toto, $tata = null, $titi = 'mieux')
+				{
+					$args = func_get_args();
+					array_shift($args);
+					return LMNC\method_parameters_as_map($this, __FUNCTION__, $args);
+				}
+			};
+
+		$this->expectException(\ReflectionException::class);
+		$result = $c->something(0);
+	}
 }
