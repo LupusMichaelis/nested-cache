96ebf4a647ae28b8ed1134fbd46ef8919b87a31c
Mickaël Wolff <mickael@lupusmic.org>
2021-05-08 21:42:34 +0200

    Add a mechanism to allow choosing persistent cache class

--- src/KeeperInterface.php
+++ src/KeeperInterface.php
@@ -4,5 +4,6 @@ namespace LupusMichaelis\NestedCache;
 
 interface KeeperInterface
 {
+	function set_persistent_cache_class(string $class_name): void;
 	function add_group(string $name, bool $is_persistent): void;
 }
--- src/ObjectCacheKeeper.php
+++ src/ObjectCacheKeeper.php
@@ -24,6 +24,11 @@ class ObjectCacheKeeper
 			$this->non_persistent_group_list[(string) $name] = true;
 	}
 
+	public function set_persistent_cache_class(string $class_name): void
+	{
+		$this->peristent_cache_class = $class_name;
+	}
+
 	public function get(Key\Cut $key)
 	{
 		$cache = $this->get_cache_for_key($key);
@@ -83,15 +88,30 @@ class ObjectCacheKeeper
 			$this->cache_list[$key->get_blog_id()] = [];
 
 		if(!isset($this->cache_list[$key->get_blog_id()][$key->get_group()]))
-			$this->cache_list[$key->get_blog_id()][$key->get_group()] = new ObjectCache\BareArray;
+			$this->instantiate_cache_for_group($key);
 
 		return $this->cache_list[$key->get_blog_id()][$key->get_group()];
+	}
+
+	private function is_persistent(Key\Cut $key)
+	{
+		return isset($this->persistent_group_list[$key->get_name()]);
+	}
+
+	private function instantiate_cache_for_group(Key\Cut $key)
+	{
+		$class = $this->is_persistent($key)
+			? $this->peristent_cache_class
+			: ObjectCache\BareArray::class
+			;
+		$this->cache_list[$key->get_blog_id()][$key->get_group()] = new $class;
 	}
 
 	private $default_group_name = WordPress\ObjectCacheInterface::default_group_name;
 
 	private $cache_list = []; ///< @property array $cache_list
 
+	private $peristent_cache_class = ObjectCache\BareArray::class; ///< @property string
 	private $non_persistent_group_list = []; ///< @property bool[string]
 	private $persistent_group_list = []; ///< @property bool[string]
 	private $stats; ///< @property \LupusMichaelis\NestedCache\StatInterface
