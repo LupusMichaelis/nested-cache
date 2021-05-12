2306d313cb23c9eff60cd6274748eb43630662fc
Mickaël Wolff <mickael@lupusmic.org>
2021-05-12 23:44:17 +0200

    Test that group marked as persistent twice isn't dropping the cache

---
+++ tests/ObjectCacheKeeperTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace LupusMichaelis\NestedCache\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ObjectCacheKeeperTest
+	extends TestCase
+{
+	public function setUp(): void
+	{
+		$this->cache_keeper = new \LupusMichaelis\NestedCache\ObjectCacheKeeper;
+	}
+
+	public function tearDown(): void
+	{
+		unset($this->cache_keeper);
+		\apcu_clear_cache();
+	}
+
+	public function provideKeys()
+	{
+		return
+			[ [new \LupusMichaelis\NestedCache\Key\Cut(['blog_id' => 1, 'group' => 'bundle', 'name' => 'target'])]
+			];
+	}
+
+	public function testInstantiate()
+	{
+		$this->assertInstanceOf(\LupusMichaelis\NestedCache\ObjectCacheKeeper::class, $this->cache_keeper);
+	}
+
+	/**
+	 * @dataProvider provideKeys
+	 */
+	public function testChangeGroupWithoutChangingKeepContent($key)
+	{
+		$this->cache_keeper->add_group('bundle', true);
+
+		$this->cache_keeper->set($key, 42);
+		$value = $this->cache_keeper->get($key);
+		$this->assertEquals(42, $value);
+
+		$this->cache_keeper->add_group('bundle', true);
+		$value = $this->cache_keeper->get($key);
+		$this->assertEquals(42, $value);
+	}
+}
