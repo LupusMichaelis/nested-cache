3840ccb6ad45b1e8cd8935a4e4d5fcd03c318ce9
Mickaël Wolff <mickael@lupusmic.org>
2021-06-02 18:34:13 +0200

    Helper function to complete an array of values with default function parameter values

--- src/free-functions.php
+++ src/free-functions.php
@@ -9,20 +9,37 @@ namespace LupusMichaelis\NestedCache;
  * @throws \ReflectionException	If array of values is too short and there is no default value
  * @return array[mixed]
  */
-function method_parameters_as_map(object $that, string $function_name, array & $values)
+function method_parameters_as_map(object $that, string $function_name, array & $values): array
 {
 	$object_glimpse = new \ReflectionClass($that);
 	$function_glimpse = $object_glimpse->getMethod($function_name);
-	$parameter_list = $function_glimpse->getParameters();
+	return function_glimpse_parameters_as_map($function_glimpse, $values);
+}
 
+function function_parameters_as_map(string $function_name, array & $values): array
+{
+	$function_glimpse = new \ReflectionFunction($function_name);
+	return function_glimpse_parameters_as_map($function_glimpse, $values);
+}
+
+function function_glimpse_parameters_as_map(\ReflectionFunctionAbstract $function_glimpse, array & $values): array
+{
+	$parameter_list = $function_glimpse->getParameters();
 	$count_values = count($values);
 	$value_list = [];
 	foreach($parameter_list as $parameter_position => $parameter_glimpse)
-		$value_list[$parameter_glimpse->getName()] =
-			$parameter_position < $count_values
-				?  $values[$parameter_position]
-				: $parameter_glimpse->getDefaultValue()
-				;
+		if($parameter_position < $count_values)
+			$value_list[$parameter_glimpse->getName()] = $values[$parameter_position];
+		else
+			if($parameter_glimpse->isPassedByReference())
+				$value_list[$parameter_glimpse->getName()] = &by_ref($parameter_glimpse->getDefaultValue());
+			else
+				$value_list[$parameter_glimpse->getName()] = $parameter_glimpse->getDefaultValue();
 
 	return $value_list;
+}
+
+function &by_ref($value)
+{
+	return $value;
 }
